dbb283b46bb093c3af1c98890036a8640335b194
sachin kumar <kumarsachin88131@gmail.com>
2025-08-20 13:59:05 +0530

    commit first

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 main().then(
-    console.log("database successfuly create")
+    ()=>console.log("database successfuly create")
 ).catch(err => console.log(err))
 
 async function main(){
-  await  mongoose.connect("mongodb://127.0.0.1:27017/olx")
+  await  mongoose.connect("mongodb+srv://kumarsachin88131:9ajSeET6FdQXxCVg@cluster0.mgapbil.mongodb.net/olx")
 }
