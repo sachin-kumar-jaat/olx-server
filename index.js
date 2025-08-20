a42ec71626a99e672f3521d8bc9817a976fb67dc
sachin kumar <kumarsachin88131@gmail.com>
2025-08-20 08:23:28 +0530

    commit first

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
 
+
 const express = require("express")
 const cors =  require ("cors")
 const app = express()
-PORT = 5000;
-require("./db/dbconnect")
-const router = require("./Router/router")
-const serverless = require("serverless-http")
 
-app.use(cors(
-    {
-        origin: "http://localhost:5173"
-      }
-))
+require("./db/dbconnect")
+require("dotenv").config()
+const router = require("./Router/router")
+
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use("/api/auth",router)
 
-module.exports = app
-module.exports.handler = serverless(app)
-
-
-
-// const express = require("express")
-// const cors =  require ("cors")
-// const app = express()
-// PORT = 5000;
-// require("./db/dbconnect")
-// const router = require("./Router/router")
-
-
-// app.use(cors(
-//     {
-//         origin: "http://localhost:5173"
-//       }
-// ))
-// app.use(express.json())
-// app.use(express.urlencoded({extended:false}))
-
-// app.use("/api/auth",router)
-
-// app.listen(PORT,()=>{
-//     console.log("successful create server")
-// })
+app.listen(process.env.PORT,()=>{
+    console.log("successful create server")
+})
