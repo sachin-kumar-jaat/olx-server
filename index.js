611cb1a5b06ebc41a4513fbd6be4b74d867e468e
sachin kumar <kumarsachin88131@gmail.com>
2025-08-20 11:11:46 +0530

    commit first

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ require("dotenv").config()
 const router = require("./Router/router")
 
 
-app.use(cors(
-    {
-        origin:"https://mern-olx-project.netlify.app/"
-    }
-))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
