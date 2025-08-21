3283d8303afd344b1daeaf16b8af3f01af5c7ebd
sachin kumar <kumarsachin88131@gmail.com>
2025-08-21 09:28:41 +0530

    commit folxiuy

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -5,5 +5,5 @@ main().then(
 ).catch(err => console.log(err))
 
 async function main(){
-  await  mongoose.connect("mongodb+srv://kumarsachin88131:Gaa10NNjAuGbaD0O@cluster0.p46l9zy.mongodb.net//olx")
+  await  mongoose.connect("mongodb+srv://kumarsachin88131:Gaa10NNjAuGbaD0O@cluster0.p46l9zy.mongodb.net/olx")
 }
